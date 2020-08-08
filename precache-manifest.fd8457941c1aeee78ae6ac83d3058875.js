0a379a70828c0416046fdd5b776cfdfd00fe1e01
Sandiprghane <64368583+Sandiprghane@users.noreply.github.com>
2020-08-08 20:49:27 +0530

    Updates

---
+++ precache-manifest.fd8457941c1aeee78ae6ac83d3058875.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b7179ad44bb65a6be8349f02c88c87",
+    "url": "/goForFresh/index.html"
+  },
+  {
+    "revision": "698fa66d3b5e149197ba",
+    "url": "/goForFresh/static/css/main.0f5bc524.chunk.css"
+  },
+  {
+    "revision": "661ed1695a27a7e325d8",
+    "url": "/goForFresh/static/js/2.502c1153.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/goForFresh/static/js/2.502c1153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698fa66d3b5e149197ba",
+    "url": "/goForFresh/static/js/main.479ddbe4.chunk.js"
+  },
+  {
+    "revision": "50eda69205aea990a939",
+    "url": "/goForFresh/static/js/runtime-main.9daa4343.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/goForFresh/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/goForFresh/static/media/shopping-bag.30f07071.svg"
+  }
+]);
